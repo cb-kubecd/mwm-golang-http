cf2bb6a8c8ae87adfa442f0310f295ccb0b18b8c
Mark Wynn-Mackenzie <mwynnmackenzie@cloudbees.com>
2019-02-05 16:07:51 +0000

    small update 7

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example-9"
+	title := "Jenkins X golang http example-11"
 
 	from := ""
 	if r.URL != nil {
